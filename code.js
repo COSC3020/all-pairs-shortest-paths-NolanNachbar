fa43c1c79a5311fcec59433b0714d51df048637a
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-09 15:37:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,23 @@
 function allPairsShortestPaths(graph) {
-    return [];
-}
+  
+  let card = graph.length; 
+  let dist = Array.from({ length: card}, () => Array(card).fill(Infinity));
+  
+  for(let i = 0; i < card; i++) dist[i][i] = 0;
+    
+  for(let v = 0; v < card; v++){
+    for(let u = 0; u < card; u++){
+      dist[u][v] = graph[u][v];
+    }
+  }
+  
+  for(let k = 0; k < card; k++){
+    for(let i = 0; i < card; i++){
+      for(let j = 0; j < card; j++){
+        if (dist[i][j] > dist[i][k] + dist[k][j]) dist[i][j] = dist[i][k] + dist[k][j];
+      }
+    }
+  }
+return dist;
 
+}
